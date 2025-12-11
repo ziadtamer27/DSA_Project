709a973250d1e7c3cde844a66977b106f24d9313
Mazen Mohamed <mazenelbitar@hotmail.com>
2025-12-11 16:54:03 +0200

    cpp file

--- src/code/main.cpp
+++ src/code/main.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #include "..\\header\\CheckXmlFile.h"
 #include "..\\header\\CompressingXML.h"
-
+// hello world
 using namespace std;
 
 string readFile(const string& path) {
