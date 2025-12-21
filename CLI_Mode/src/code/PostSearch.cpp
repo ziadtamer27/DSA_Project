a7b6d7a836fc1d8c391975c4abc93c23dee418d2
Menna Mohamed <alahmena354@gmail.com>
2025-12-22 01:25:12 +0200

    Post Search Topic

--- CLI_Mode/src/code/PostSearch.cpp
+++ CLI_Mode/src/code/PostSearch.cpp
@@ -43,3 +43,47 @@ string PostSearchWord(string xml_file, string word) {
     if(posts.size()>0)return posts;
     else return "Word Not Found In Any Post";
 }
+
+string PostSearchTopic(string xml_file, string topic) {
+    xml_file = Minifyingxmlfile(xml_file);
+    topic = topic + ' ';
+    string posts = "";
+    string post = "";
+    string t ="";
+    bool flag = false;
+    string tag = "";
+    int counter = 1;
+    for (int i = 0; i < xml_file.size(); i++) {
+        if (xml_file[i] == '<') {
+            flag = true;
+        }
+        else if (flag) {
+            tag += xml_file[i];
+        }
+        if (xml_file[i] == '>') {
+            if (tag == "body>") {
+                int x = i + 1;
+                post = "";
+                while (xml_file[x] != '<') {
+                        post += xml_file[x++];
+                }
+            }
+            if (tag == "topic>")
+            {
+                int x = i + 1;
+                t = "";
+                while (xml_file[x] != '<') {
+                    t += xml_file[x++];
+                }
+                if(topic == t){
+                    posts += "Post "+ to_string(counter++)+":" + "\n";
+                    posts += post + "\n";
+                }
+            }
+            flag = false;
+            tag = "";
+        }
+    }
+    if(posts.size()>0)return posts;
+    else return "topic Not Found In Any Post";
+}
--- CLI_Mode/src/header/PostSearch.h
+++ CLI_Mode/src/header/PostSearch.h
@@ -1,3 +1,4 @@
 #include <bits/stdc++.h>
 using namespace std;
 string PostSearchWord(string xml_file,string word);
+string PostSearchTopic(string xml_file,string topic);
