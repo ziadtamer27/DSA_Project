a8a012cf9a32838a373b6b8a7a4ca263415d3f87
Shahd Bassem <shahdbassem1@gmail.com>
2025-12-23 23:14:10 +0200

    mostInfluencer

--- CLI_Mode/src/header/NetworkAnalysis.h
+++ CLI_Mode/src/header/NetworkAnalysis.h
@@ -1,3 +1,4 @@
 #include <bits/stdc++.h>
 using namespace std;
 string suggestUser(int userid, vector<pair<int,vector<int>>> connections);
+string mostInfluencer(vector<pair<int, vector<int>>> connections);
