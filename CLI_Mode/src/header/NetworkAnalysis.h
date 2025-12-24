0d955d1b8861c4938be272d5b917829f50ceb37a
Shahd Bassem <shahdbassem1@gmail.com>
2025-12-24 13:51:32 +0200

    Update NetworkAnalysis.h

--- CLI_Mode/src/header/NetworkAnalysis.h
+++ CLI_Mode/src/header/NetworkAnalysis.h
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 string suggestUser(int userid, vector<pair<int,vector<int>>> connections);
-string mostInfluencer(vector<pair<int, vector<int>>> connections);
+string mutualUsers(int user1, int user2, vector<pair<int, vector<int>>> connections);
+string mostInfluencer(vector<pair<int, vector<int>>> connections, const vector<User>& users);
 pair<int, string> mostActiveUser(const vector<pair<int, vector<int>>>& graph, const vector<User>& users);
